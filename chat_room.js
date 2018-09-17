108c5774da92270c58fcfcb7c1b89677f7439617
jerrello <43283084+jerrello@users.noreply.github.com>
2018-09-17 14:34:38 -0400

    Update to chat_room.js
    Fixed event listener load on document ready.

--- chat_room.js
+++ chat_room.js
@@ -1,9 +1,11 @@
-$("#submit_post_btn").click(function () {
-	chat= document.getElementById("msg_list");
-	msg=	document.createElement('p');
-	msg.innerHTML= document.getElementById("post").value;
-	chat.appendChild(msg);
-	msg.style= "text-align:right;";
-	socket.send(document.getElementById("post").value);
-	document.getElementById("post").value = "";
+$(document).ready(function () {
+	$("#submit_post_btn").click(function () {
+		chat= document.getElementById("msg_list");
+		msg=	document.createElement('p');
+		msg.innerHTML= document.getElementById("post").value;
+		chat.appendChild(msg);
+		msg.style= "text-align:right;";
+		socket.send(document.getElementById("post").value);
+		document.getElementById("post").value = "";
+	});
 });
