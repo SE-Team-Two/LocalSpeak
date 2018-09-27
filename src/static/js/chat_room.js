0302ab7e267a9b60db187f155e2fda5397781483
vzyrianov <vzyriano@kent.edu>
2018-09-27 19:49:12 -0400

    Make messages be recieved from the server

--- src/static/js/chat_room.js
+++ src/static/js/chat_room.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
+   var socket = io.connect('http://' + document.domain + ":" + location.port);
+
 	$("#submit_post_btn").click(function () {
 		chat= document.getElementById("msg_list");
 		msg=	document.createElement('p');
 		msg.innerHTML= document.getElementById("post").value;
 		chat.appendChild(msg);
 		msg.style= "text-align:right;";
-		socket.send(document.getElementById("post").value);
-		document.getElementById("post").value = "";
+      socket.send(document.getElementById("post").value);
+      document.getElementById("post").value = "";
 	});
+
+   socket.on('message', function(msg) {
+      chat = document.getElementById("msg_list");
+		displayedMessage = document.createElement('p');
+		displayedMessage.innerHTML= msg;
+		chat.appendChild(displayedMessage);
+
+   });
 });
