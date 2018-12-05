51495975892b559f7391b4625ca40ef6c1fa6003
Ian Whitesel <iwhitese@kent.edu>
2018-12-04 20:44:08 -0500

    Uploading styling for updated version

--- pusher/server.py
+++ pusher/server.py
@@ -3,7 +3,9 @@ from flask_session import Session;
 from flask_mysqldb import MySQL
 from pusher import Pusher;
 from datetime import datetime, timedelta
+from geopy import distance
 import json
+import mysql.connector
 
 app = Flask(__name__);
 SECRET_KEY = 'secretytypeword!';
@@ -27,14 +29,25 @@ pusher = Pusher(
 @app.route("/",methods=['POST','GET'])
 def login():
 	if request.method == "POST":
-		session['username'] = request.form['username'];
-		session['room'] = 'Global';
-		return redirect(url_for("chat",username=session['username'])); 
+		username = request.form['username'];
+		password = request.form['password'];
+		cursor = mysql.connection.cursor()
+		query = "SELECT * FROM users WHERE username ='" + username +"';"
+		cursor.execute(query)
+		data = cursor.fetchall()
+		if data:
+			if data[0][2] == password:
+				session['username'] = username;
+				session['distance'] = data[0][3];
+				return redirect(url_for("chat",username=session['username']));
+			else:
+				return render_template("login.html",error=1);
+		else:
+			return render_template("login.html",error=1);
 	elif 'username' in session:
-		session['room'] = 'Global';
 		return redirect(url_for("chat",username=session['username'])); 
 	else:
-		return render_template("login.html");
+		return render_template("login.html",error=0);
 
 @app.route("/chat/<username>")
 def chat(username):
@@ -43,7 +56,7 @@ def chat(username):
 	else:
 		return render_template("login.html");
 
-@app.route("/settings/<username>")
+@app.route("/settings/<username>",methods=["POST","GET"])
 def settings(username):
 	if 'username' in session and session['username'] == username:
 		return render_template("settings.html",curr_usr=username);
@@ -64,61 +77,120 @@ def message(room):
 	else:
 		return jsonify(username=username,msg='Message Failed!')
 
-@app.route("/create/<room>",methods=['POST'])
-def create(room):
-	username = request.form['username']
-	if 'username' in session and session['username'] == username:
-		channels = pusher.channels_info()
-		pusher.trigger(channels['channels'].keys(),'new_room',{'room':room,'username':username})
-		name = request.form['name']
-		delta = request.form['time']
-		time = datetime.now() + timedelta(hours=int(delta))
-		query= "INSERT INTO rooms VALUES (default,'"+name+"','"+str(time)+"','"+username+"');"
+@app.route("/create/<call>",methods=['POST'])
+def create(call):
+	if call == "room":
+		username = request.form['username']
+		if 'username' in session and session['username'] == username:
+			name = request.form['name']
+			delta = request.form['time']
+			time = datetime.now() + timedelta(hours=int(delta))
+			query= "INSERT INTO rooms VALUES (default,'"+name+"','"+str(time)+"','"+username+"',"+session['lat']+","+session['long']+");"
+			cursor = mysql.connection.cursor()
+			cursor.execute(query)
+			mysql.connection.commit();
+			query = "SELECT * FROM users;"
+			cursor.execute(query)
+			response = cursor.fetchall()
+			data = list()
+			r_position = (session['lat'],session['long'])
+			for user in response:
+				u_position = (user[4],user[5])
+				u_distance = user[3]
+				if distance.distance(r_position,u_position).miles <= u_distance:
+					data.append(user[1]);
+			for user in data:
+				pusher.trigger('private-'+user,'new_room',{'room':name,'username':username})
+			return jsonify(msg='success')
+	if call == "user":
+		username = request.form['username']
+		query= "SELECT * FROM users WHERE username = '"+username+"';"
+		cursor = mysql.connection.cursor()
+		cursor.execute(query)
+		data = cursor.fetchall()
+		if data:
+			return jsonify(msg='fail')
+		else:
+			password = request.form['password']
+			query= "INSERT INTO users VALUES (default,'"+username+"','"+password+"',default,"+request.form['lat']+","+request.form['long']+");"
+			cursor = mysql.connection.cursor()
+			cursor.execute(query)
+			mysql.connection.commit();
+			session['username'] = username
+			return jsonify(msg='success')
+
+@app.route("/save/<call>",methods=['POST'])
+def save(call):
+	if call == "user":
+		session['username'] = request.form['username']
+		session['lat'] = request.form['lat']
+		session['long'] = request.form['long']
+		query = "UPDATE users SET latitude = "+request.form['lat']+", longitude ="+request.form['long']+" WHERE username = '"+request.form['username']+"';"
 		cursor = mysql.connection.cursor()
 		cursor.execute(query)
 		mysql.connection.commit();
-		return jsonify(msg='Success!')
-
-@app.route("/save",methods=['POST'])
-def save():
-	session['username'] = request.form['username']
-	session['SID'] = request.form['SID']
-	return jsonify(msg='Success!')
+		return jsonify(msg='success')
+	if call == "distance":
+		session['distance'] = request.form['distance']
+		query = "UPDATE users SET distance = "+request.form['distance']+" WHERE username = '"+request.form['username']+"';"
+		cursor = mysql.connection.cursor()
+		cursor.execute(query)
+		mysql.connection.commit();
+		return jsonify(msg='success')
+	if call == "password":
+		password = request.form['password'];
+		cursor = mysql.connection.cursor()
+		query = "UPDATE users SET password = '"+password+"' WHERE username = '"+request.form['username']+"';"
+		cursor.execute(query)
+		mysql.connection.commit();
+		return jsonify(msg='success')
 
 @app.route("/authenticate",methods=['POST'])
 def authenticate():
   auth = pusher.authenticate( channel=request.form['channel_name'],
-			     socket_id=request.form['socket_id'],custom_data= {'user_id':session['username']})
+  														socket_id=request.form['socket_id'],
+  														custom_data= {'user_id':session['username']})
   return json.dumps(auth)
 
-@app.route("/get/<request>",methods=['POST'])
-@app.route("/get/<request>/<room>",methods=['POST'])
-def get(request,room=None):
-	if request == "messages":
-		cursor = mysql.connection.cursor()
-		query = "SELECT * FROM messages WHERE room ='" + room +"' ORDER BY time DESC LIMIT 50;"
-		cursor.execute(query)
-		data = cursor.fetchall()
-		return jsonify(data=data)
-	elif request == "rooms":
-		cursor = mysql.connection.cursor()
-		query = "SELECT * FROM rooms;"
-		cursor.execute(query)
-		data = cursor.fetchall()
-		return jsonify(data=data)
+@app.route("/get/<call>",methods=['POST'])
+@app.route("/get/<call>/<room>",methods=['POST'])
+def get(call,room=None):
+	username = request.form['username']
+	if 'username' in session and session['username'] == username: 
+		if call == "messages":
+			cursor = mysql.connection.cursor()
+			query = "SELECT * FROM messages WHERE room ='" + room +"' ORDER BY time DESC LIMIT 50;"
+			cursor.execute(query)
+			data = cursor.fetchall()
+			return jsonify(data=data)
+		elif call == "rooms":
+			cursor = mysql.connection.cursor()
+			query = "SELECT * FROM rooms;"
+			cursor.execute(query)
+			response = cursor.fetchall()
+			data = list()
+			u_position = (request.form['lat'],request.form['long'])
+			for room in response:
+				r_position = (room[4],room[5])
+				if distance.distance(r_position,u_position).miles <= int(session['distance']):
+					data.append(room);
+			return jsonify(data=data)
+		elif call == "distance":
+			cursor = mysql.connection.cursor()
+			query = "SELECT distance FROM users where username='"+username+"';"
+			cursor.execute(query)
+			data = cursor.fetchall()
+			return jsonify(data=data)
 
 @app.route("/logout")
 def logout():
-	session.pop('username');
-	session.pop('SID',None);
+	session.pop('username',None);
+	session.pop('lat',None);
+	session.pop('long',None);
 	return redirect(url_for('login'));
 
 def mk_room(room):
 	return 'presence-'+room
-
-# @app.route("/testing")
-# def testing():
-# 	return render_template("testing.html",string=str(request.cookies['session']))
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- pusher/sql/speakeasy.sql
+++ pusher/sql/speakeasy.sql
@@ -9,7 +9,9 @@ CREATE TABLE users (
 user_id INT(6) ZEROFILL AUTO_INCREMENT PRIMARY KEY,
 username VARCHAR(32) NOT NULL UNIQUE,
 password VARCHAR(32) NOT NULL,
-distance SMALLINT(2) NOT NULL DEFAULT 25
+distance SMALLINT(2) NOT NULL DEFAULT 25,
+latitude FLOAT(32,30) NOT NULL,
+longitude FLOAT(32,30) NOT NULL
 );
 
 CREATE TABLE messages (
@@ -24,7 +26,9 @@ CREATE TABLE rooms (
 room_id INT(6) ZEROFILL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(32) NOT NULL UNIQUE,
 expire DATETIME NOT NULL,
-creator VARCHAR(32) NOT NULL
+creator VARCHAR(32) NOT NULL,
+latitude FLOAT(32,30) NOT NULL,
+longitude FLOAT(32,30) NOT NULL
 );
 
 ALTER TABLE messages
@@ -38,15 +42,15 @@ REFERENCES users(username)
 ON DELETE CASCADE;
 
 INSERT INTO users VALUES 
-(default,'User1','password1',default),
-(default,'User2','password2',default),
-(default,'User3','password3',default),
-(default,'User4','password4',default),
-(default,'User5','password5',default),
-(default,'User6','password6',default),
-(default,'User7','password7',default),
-(default,'User8','password8',default),
-(default,'User9','password9',default);
+(default,'User1','password1',default,41.147548,-81.343118),
+(default,'User2','password2',default,41.147548,-81.343118),
+(default,'User3','password3',default,41.147548,-81.343118),
+(default,'User4','password4',default,41.147548,-81.343118),
+(default,'User5','password5',default,41.147548,-81.343118),
+(default,'User6','password6',default,41.147548,-81.343118),
+(default,'User7','password7',default,41.147548,-81.343118),
+(default,'User8','password8',default,41.147548,-81.343118),
+(default,'User9','password9',default,41.147548,-81.343118);
 
 -- CREATE USER 'my_daemon'@'localhost' IDENTIFIED BY 'daemondothething!';
 
